6ee8539f5c668aae8d0e5af933bff28963733481
Christian Awelakoue <awelakoue@gmail.com>
2024-05-17 10:00:55 +0200

    template literals

--- 02-JS-review/script.js
+++ 02-JS-review/script.js
@@ -185,3 +185,6 @@ const updatedBook = {
   pages: 1210,
 };
 updatedBook;
+// template literals
+const summary = `${title}, a ${pages}-page long book, was written by ${author} and published in ${publicationDate.split('-')[0]}`;
+summary;
